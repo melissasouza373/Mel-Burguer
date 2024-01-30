b1d6f4279b3d7d4b72a4c8d14a6482fe32c4acd0
melissasouza <melissasouza373@gmail.com>
2024-01-30 18:45:30 -0300

    Projeto

---
+++ products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 20, vegan: false, src: './img/xsalada.jpeg' },
+    { name: 'X-Bacon', price: 30, vegan: false, src: './img/xbacon.png' },
+    { name: 'X-Bacon Egg', price: 35, vegan: false, src: './img/bacon-egg.png' },
+    { name: 'Monstruoso', price: 40, vegan: false, src: './img/monstruoso.png' },
+    { name: 'Big Vegano', price: 45, vegan: true, src: './img/xvegan.png' },
+    { name: 'X-Vegan', price: 38, vegan: true, src: './img/monstruoso-vegan.png' },
+    ]
